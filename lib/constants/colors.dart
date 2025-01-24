7823b604580adfa0dbcb3724ccf805aef5f54a53
adoniyas7 <adoniyasg7s@gmail.com>
2025-01-24 23:09:33 +0300

    Add theme colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryBlue = Color(0xFF2AABEE);
+  static const secondaryBlue = Color(0xFF2AABEE);
+  static const lightGrey = Color(0xFFF2F2F2);
+  static const darkGrey = Color(0xFF8E8E93);
+  static const onlineGreen = Color(0xFF4CAF50);
+
+  static const grey = Color(0xFFBDBDBD);
+}
